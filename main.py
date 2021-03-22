71e2703819e6368a4027f0be7ec75bfc04b67caa
Thomas Lee <tcl1701@gmail.com>
2021-03-22 09:06:18 +0000

    Trying to fix main right now. Split() is splitting the message up too much.

--- main.py
+++ main.py
@@ -21,19 +21,19 @@ sends a message to that channel
 async def on_message(message):
   if message.author == client.user:
     return
-  if not message.startswith('//'):
+  if not message.content.startswith('//'):
     return
   message_contents = message.content.split()
-  if message_contents[0] + " " + message_contents[1].lower() == ADD_REMINDER_COMMAND:
+  if len(message_contents) >= 2 and message_contents[0] + " " + message_contents[1].lower() == ADD_REMINDER_COMMAND:
     if len(message_contents) != 4:
-      message.channel.send('Incorrect operands! //add reminder *message date*')
+      await message.channel.send('Incorrect operands! //add reminder **message date**')
     else:
       added_reminder = add_reminder(message_contents[2], message_contents[3])
       if added_reminder:
         message_to_send = "Successfully added reminder...\n{message}\n{reminder_date}".format(message=added_reminder.date, reminder_date=added_reminder.month + '-' + added_reminder.day + '-' + added_reminder.year)
       else:
         message_to_send = 'Failed to add reminder!\nCommon mistakes include: not formatting date to be mm-dd-yyyy'
-      message.channel.send(message_to_send)
+      await message.channel.send(message_to_send)
   
 
 client.run(os.getenv('TOKEN'))
